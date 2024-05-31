8571b18275704d2c10e96cef867ffbbe5ce1bf49
Gaelle Bono <gaelle.bono@free.fr>
2024-05-31 23:33:38 +0200

    removing CloudCannon cms config and adding DecapCMS config

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 
 module.exports = function (eleventyConfig) {
 
+  eleventyConfig.addPassthroughCopy("src/admin");
+
   eleventyConfig.addPassthroughCopy('src/assets');
   eleventyConfig.addPassthroughCopy('src/uploads');
   eleventyConfig.addPassthroughCopy('src/js');
